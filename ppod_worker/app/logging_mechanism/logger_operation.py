7e9868c70baa7e7fb0ecd02ec0ecd6e4bdb12fd4
Prakash <prakash.ct@prodapt.com>
2025-10-07 02:15:53 +0530

    Fix logger error

--- ppod_worker/app/logging_mechanism/logger_operation.py
+++ ppod_worker/app/logging_mechanism/logger_operation.py
@@ -3,23 +3,26 @@ from logging.handlers import RotatingFileHandler
 
 from dotenv import load_dotenv
 import os
+
 load_dotenv()
 project_root = os.getenv("Project_Root")
+log_dir = os.path.join(os.path.dirname(__file__), "error_in_data_process.log")
 
-#logging.basicConfig(
+# logging.basicConfig(
 #    level=logging.INFO,
 #    format='%(asctime)s - %(levelname)s - %(message)s',
 #    handlers=[
 #        logging.FileHandler("error_in_data_process.log"),
 #        logging.StreamHandler()
 #    ]
-#)
+# )
 
-rotating_handler = RotatingFileHandler(f"./logging_mechanism/error_in_data_process.log", maxBytes=5_000_000, backupCount=3)
-rotating_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+rotating_handler = RotatingFileHandler(log_dir, maxBytes=5_000_000, backupCount=3)
+rotating_handler.setFormatter(
+    logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+)
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 logger.addHandler(rotating_handler)
 logger.addHandler(logging.StreamHandler())
-
--- ppod_worker/app/services/RabbitMQ_Processer.py
+++ ppod_worker/app/services/RabbitMQ_Processer.py
@@ -7,7 +7,7 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-from ant_worker import logger
+from app.logging_mechanism import logger
 
 # RMQ_HOST = "10.168.171.75"
 # RMQ_PORT = "5672" #8083
